66f37d97a9d068369b8cb5621d8633b22ba5de1a
Milan1120 <milansharma86280@gmail.com>
2024-11-05 11:12:22 +0530

    change in  location and internship

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -27,7 +27,7 @@ function Location() {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3431.1085752069794!2d76.66190047503578!3d30.68722108783171!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fef154b91a85b%3A0x4d8b9df97e986631!2sChandigarh%20Group%20of%20Colleges%20(CGC)%20-%20Landran!5e0!3m2!1sen!2sin!4v1725014568626!5m2!1sen!2sin"
             allowFullScreen
             loading="lazy"
-            style={{ width: '100%', height: '350px' }}
+            style={{ width: '70vw', height: '350px' }}
           ></iframe>
       </Box>
     </Container>
