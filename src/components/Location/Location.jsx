66576d6a6a7ca5b6cb3fcb6518963b67cc2b5983
NikhilDhiman <nikhil.dhiman7973@gmail.com>
2025-01-08 13:13:51 +0530

    Maplocationchange

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -24,7 +24,7 @@ function Location() {
       <Box className="location-container px-0 animate-fadeInUp w-full" 
         data-wow-delay="0.1s">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3430.439378385074!2d76.63500331502591!3d30.75127818162681!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fed42d751d7c9%3A0xe80265eb0a36cb87!2sRS%20GROUP%2C%20Sector%20115%2C%20Kharar%2C%20Punjab%20140301!5e0!3m2!1sen!2sin!4v1615000000000!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3430.439378385074!2d76.6470749679275!3d30.73486136820908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fed42d751d7c9%3A0xe80265eb0a36cb87!2sRS%20GROUP%2C%20Sector%20115%2C%20Kharar%2C%20Punjab%20140301!5e0!3m2!1sen!2sin!4v1615000000000!5m2!1sen!2sin"
             allowFullScreen
             loading="lazy"
             style={{ width: '70vw', height: '350px' }}
