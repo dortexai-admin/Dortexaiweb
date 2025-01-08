caf0e6cf15202caac8cde7d2d6ce2797999bbce7
NikhilDhiman <nikhil.dhiman7973@gmail.com>
2025-01-08 12:10:48 +0530

    minor changes

--- src/components/ConnectForm/ConnectForm.jsx
+++ src/components/ConnectForm/ConnectForm.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import {
-	Box,
-	Typography,
-	Button,
-	TextField,
-	Grid,
-	IconButton,
-	Divider,
-	InputAdornment,
-	Container,
+  Box,
+  Typography,
+  Button,
+  TextField,
+  Grid,
+  IconButton,
+  Divider,
+  InputAdornment,
+  Container,
 } from "@mui/material";
 
 // Import icons individually
@@ -23,278 +23,263 @@ import EmailIcon from "@mui/icons-material/Email";
 import PersonIcon from "@mui/icons-material/Person";
 import InstagramIcon from "@mui/icons-material/Instagram";
 
-import contactus from "../../assets/about/contactf.jpg"; // Ensure the path is correct
-
+import contactus from "../../assets/about/contactf.jpg";
 
 const ConnectForm = () => {
-	return (
-		<Container
-			maxWidth="lg"
-			sx={{
-				position: "relative",
-				padding: 4,
-				backgroundImage: `url(${contactus})`, // Apply background image
-				backgroundSize: "cover",
-				backgroundPosition: "center",
-				borderRadius: 4,
-				// boxShadow: 10,
-				// boxShadow: '0 4px 10px rgba(0, 1, 255, 2)', // Blue box shadow
-				display: "flex",
-				justifyContent: "center",
-				alignItems: "center",
-				minHeight: "10vh", // Full height of the viewport
-				"::before": {
-					content: '""',
-					position: "absolute",
-					top: 0,
-					left: 0,
-					width: "100%",
-					height: "100%",
-					backgroundColor: "rgba(255, 255, 255, 0.4)",
-					// backgroundColor: 'blue', // Dark overlay for reducing image visibility
-				},
-			}}
-		>
-			<Box
-				className="connect-form-container"
-				sx={{
-					padding: 4,
-					backgroundColor: "rgba(255, 255, 255, 0.95)", // Semi-transparent background for form
-					borderRadius: 2,
-					boxShadow: 10,
-					width: "100%",
-					position: "relative", // Ensure form is above the overlay
-				}}
-			>
-				<Grid container spacing={4}>
-					{/* Left side: Text and social buttons */}
-					<Grid item xs={12} md={6}>
-						<Typography
-							variant="h4"
-							fontWeight="bold"
-							color="textPrimary"
-						>
-							Let's Work Together
-						</Typography>
-						<Button
-							variant="contained"
-							color="primary"
-							startIcon={<WhatsAppIcon />}
-							sx={{
-								marginTop: 2,
-								padding: "12px 24px",
-								backgroundColor: "green", // Custom color for WhatsApp button
-								"&:hover": { backgroundColor: "#533aa7" }, // Darker shade on hover
-							}}
-							onClick={() => {
-								const message =
-									"Hi DortexAI Team,\nI want to connect to have a discussion about ";
-								const phoneNumber = "+919872094149"; // Make sure the number format is correct for WhatsApp
-								const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
-									message
-								)}`;
-								window.open(whatsappUrl, "_blank"); // Open WhatsApp in a new tab
-							}}
-						>
-							WhatsApp
-						</Button>
+  return (
+    <Container
+      maxWidth="lg"
+      sx={{
+        position: "relative",
+        padding: 4,
+        backgroundImage: `url(${contactus})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        borderRadius: 4,
+        // boxShadow: 10,
+        // boxShadow: '0 4px 10px rgba(0, 1, 255, 2)',
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "10vh",
+        "::before": {
+          content: '""',
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgba(255, 255, 255, 0.4)",
+        },
+      }}
+    >
+      <Box
+        className="connect-form-container"
+        sx={{
+          padding: 4,
+          backgroundColor: "rgba(255, 255, 255, 0.95)", // Semi-transparent background for form
+          borderRadius: 2,
+          boxShadow: 10,
+          width: "100%",
+          position: "relative", // Ensure form is above the overlay
+        }}
+      >
+        <Grid container spacing={4}>
+          {/* Left side: Text and social buttons */}
+          <Grid item xs={12} md={6}>
+            <Typography variant="h4" fontWeight="bold" color="textPrimary">
+              Let's Work Together
+            </Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={<WhatsAppIcon />}
+              sx={{
+                marginTop: 1,
+                padding: "12px 24px",
+                backgroundColor: "green", // Custom color for WhatsApp button
+                "&:hover": { backgroundColor: "#533aa7" }, // Darker shade on hover
+              }}
+              onClick={() => {
+                const message =
+                  "Hi DortexAI Team,\nI want to connect to have a discussion about ";
+                const phoneNumber = "+919872094149";
+                const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+                  message
+                )}`;
+                window.open(whatsappUrl, "_blank"); // Open WhatsApp in a new tab
+              }}
+            >
+              WhatsApp
+            </Button>
 
+            <Box mt={2}>
+              <Typography variant="h6" color="textSecondary">
+                <LocationOnIcon
+                  sx={{
+                    verticalAlign: "middle",
+                    marginRight: 1,
+                  }}
+                />
+                Office:
+              </Typography>
+              <Typography variant="body1" fontWeight="bold" color="textPrimary">
+                431, Opposite Gillco valley, Landra Kharar road, SEC 115, Sector
+                115, Kharar, Punjab 140301
+              </Typography>
+              <Divider sx={{ margin: "10px 0" }} />
+              <Typography variant="h6" color="textSecondary">
+                <EmailIcon
+                  sx={{
+                    verticalAlign: "middle",
+                    marginRight: 1,
+                  }}
+                />
+                Email:
+              </Typography>
+              <Typography variant="body1" fontWeight="bold" color="textPrimary">
+                info@dortexai.com
+              </Typography>
+              <Divider sx={{ margin: "10px 0" }} />
+              <Typography variant="h6" color="textSecondary">
+                Follow Us:
+              </Typography>
+              <Box mt={1}>
+                <IconButton
+                  href="https://x.com/dortex_ai"
+                  target="_blank"
+                  aria-label="Twitter"
+                  size="small"
+                >
+                  <TwitterIcon sx={{ color: "#0c1274" }} />
+                </IconButton>
+                <IconButton
+                  href="https://www.facebook.com/profile.php?id=61561315643620"
+                  target="_blank"
+                  aria-label="Facebook"
+                  size="small"
+                >
+                  <FacebookIcon sx={{ color: "#0c1274" }} />
+                </IconButton>
+                <IconButton
+                  href="https://www.linkedin.com/company/dortex-ai/"
+                  target="_blank"
+                  aria-label="LinkedIn"
+                  size="small"
+                >
+                  <LinkedInIcon sx={{ color: "#0c1274" }} />
+                </IconButton>
+                <IconButton
+                  href="https://www.instagram.com/dortex_ai/?hl=en"
+                  target="_blank"
+                  aria-label="Instagram"
+                  size="small"
+                >
+                  <InstagramIcon sx={{ color: "#0c1274" }} />
+                </IconButton>
+              </Box>
+            </Box>
+          </Grid>
 
-						<Box mt={4}>
-							<Typography variant="h6" color="textSecondary">
-								<LocationOnIcon
-									sx={{
-										verticalAlign: "middle",
-										marginRight: 1,
-									}}
-								/>
-								Office:
-							</Typography>
-							<Typography
-								variant="body1"
-								fontWeight="bold"
-								color="textPrimary"
-							>
-								Chandigarh Group of College, Landran Mohali
-								Punjab
-							</Typography>
-							<Divider sx={{ margin: "10px 0" }} />
-							<Typography variant="h6" color="textSecondary">
-								<EmailIcon
-									sx={{
-										verticalAlign: "middle",
-										marginRight: 1,
-									}}
-								/>
-								Email:
-							</Typography>
-							<Typography
-								variant="body1"
-								fontWeight="bold"
-								color="textPrimary"
-							>
-								info@dortexai.com
-							</Typography>
-							<Divider sx={{ margin: "10px 0" }} />
-							<Typography variant="h6" color="textSecondary">
-								Follow Us:
-							</Typography>
-							<Box mt={1}>
-								<IconButton
-									href="https://x.com/dortex_ai"
-									target="_blank"
-									aria-label="Twitter"
-									size="small"
-								>
-									<TwitterIcon sx={{ color: "#0c1274" }} />
-								</IconButton>
-								<IconButton
-									href="https://www.facebook.com/profile.php?id=61561315643620"
-									target="_blank"
-									aria-label="Facebook"
-									size="small"
-								>
-									<FacebookIcon sx={{ color: "#0c1274" }} />
-								</IconButton>
-								<IconButton
-									href="https://www.linkedin.com/company/dortex-ai/"
-									target="_blank"
-									aria-label="LinkedIn"
-									size="small"
-								>
-									<LinkedInIcon sx={{ color: "#0c1274" }} />
-								</IconButton>
-								<IconButton
-									href="https://www.instagram.com/dortex_ai/?hl=en"
-									target="_blank"
-									aria-label="Instagram"
-									size="small"
-								>
-									<InstagramIcon sx={{ color: "#0c1274" }} />
-								</IconButton>
-							</Box>
-						</Box>
-					</Grid>
-
-					{/* Right side: Form */}
-					<Grid item xs={12} md={6}>
-						<form id="contact-form">
-							<Grid container spacing={2}>
-								<Grid item xs={12} md={6}>
-									<TextField
-										required
-										fullWidth
-										id="name"
-										label="Your Name"
-										variant="outlined"
-										InputLabelProps={{
-											style: {
-												fontWeight: "bold",
-												color: "black",
-											}, // Bold label
-										}}
-										InputProps={{
-											startAdornment: (
-												<InputAdornment position="start">
-													<PersonIcon />
-												</InputAdornment>
-											),
-										}}
-									/>
-								</Grid>
-								<Grid item xs={12} md={6}>
-									<TextField
-										required
-										fullWidth
-										id="email"
-										label="Your Email"
-										variant="outlined"
-										type="email"
-										InputLabelProps={{
-											style: {
-												fontWeight: "bold",
-												color: "black",
-											}, // Bold label
-										}}
-										InputProps={{
-											startAdornment: (
-												<InputAdornment position="start">
-													<EmailIcon />
-												</InputAdornment>
-											),
-										}}
-									/>
-								</Grid>
-								<Grid item xs={12}>
-									<TextField
-										required
-										fullWidth
-										id="Phone"
-										label="Phone"
-										variant="outlined"
-										InputLabelProps={{
-											style: {
-												fontWeight: "bold",
-												color: "black",
-											}, // Bold label
-										}}
-										InputProps={{
-											startAdornment: (
-												<InputAdornment position="start">
-													<SubjectIcon />
-												</InputAdornment>
-											),
-										}}
-									/>
-								</Grid>
-								<Grid item xs={12}>
-									<TextField
-										required
-										fullWidth
-										id="message"
-										label="How Can We Help You"
-										variant="outlined"
-										multiline
-										rows={4}
-										InputLabelProps={{
-											style: {
-												fontWeight: "bold",
-												color: "black",
-											}, // Bold label
-										}}
-										InputProps={{
-											startAdornment: (
-												<InputAdornment position="start">
-													<MessageIcon />
-												</InputAdornment>
-											),
-										}}
-									/>
-								</Grid>
-								<Grid item xs={12}>
-									<Button
-										variant="contained"
-										color="primary"
-										type="submit"
-										sx={{
-											width: "100%",
-											backgroundColor: "#0c1274", // Custom color for WhatsApp button
-											fontWeight: "bold",
-											"&:hover": {
-												backgroundColor: "#533aa7",
-											},
-										}}
-									>
-										Send Message
-									</Button>
-								</Grid>
-							</Grid>
-						</form>
-					</Grid>
-				</Grid>
-			</Box>
-		</Container>
-	);
+          {/* Right side: Form */}
+          <Grid item xs={12} md={6}>
+            <form id="contact-form">
+              <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="name"
+                    label="Your Name"
+                    variant="outlined"
+                    InputLabelProps={{
+                      style: {
+                        fontWeight: "bold",
+                        color: "black",
+                      }, // Bold label
+                    }}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <PersonIcon />
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Your Email"
+                    variant="outlined"
+                    type="email"
+                    InputLabelProps={{
+                      style: {
+                        fontWeight: "bold",
+                        color: "black",
+                      }, // Bold label
+                    }}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <EmailIcon />
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="Phone"
+                    label="Phone"
+                    variant="outlined"
+                    InputLabelProps={{
+                      style: {
+                        fontWeight: "bold",
+                        color: "black",
+                      }, // Bold label
+                    }}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <SubjectIcon />
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="message"
+                    label="How Can We Help You"
+                    variant="outlined"
+                    multiline
+                    rows={4}
+                    InputLabelProps={{
+                      style: {
+                        fontWeight: "bold",
+                        color: "black",
+                      }, // Bold label
+                    }}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <MessageIcon />
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    sx={{
+                      width: "100%",
+                      backgroundColor: "#0c1274", // Custom color for WhatsApp button
+                      fontWeight: "bold",
+                      "&:hover": {
+                        backgroundColor: "#533aa7",
+                      },
+                    }}
+                  >
+                    Send Message
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  );
 };
 
 export default ConnectForm;
--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -24,7 +24,7 @@ function Location() {
       <Box className="location-container px-0 animate-fadeInUp w-full" 
         data-wow-delay="0.1s">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3431.1085752069794!2d76.66190047503578!3d30.68722108783171!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fef154b91a85b%3A0x4d8b9df97e986631!2sChandigarh%20Group%20of%20Colleges%20(CGC)%20-%20Landran!5e0!3m2!1sen!2sin!4v1725014568626!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3430.439378385074!2d76.63500331502591!3d30.75127818162681!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fed42d751d7c9%3A0xe80265eb0a36cb87!2sRS%20GROUP%2C%20Sector%20115%2C%20Kharar%2C%20Punjab%20140301!5e0!3m2!1sen!2sin!4v1615000000000!5m2!1sen!2sin"
             allowFullScreen
             loading="lazy"
             style={{ width: '70vw', height: '350px' }}
